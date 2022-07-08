b197178126ba603f33670730ed87145c1ec85f2c
Maksym <lucky13375@gmail.com>
2022-07-08 16:01:30 +0200

    Change 12
    Fixed

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -5,14 +5,41 @@
 
   menuBtnRef.addEventListener("click", () => {
     const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
-    menuBtnRef.classList.toggle("is-open");
+    menuBtnRef.getAttribute("aria-expanded") === "true";
     menuBtnRef.setAttribute("aria-expanded", !expanded);
 
     mobileMenuRef.classList.toggle("is-open");
 
     bodyClassToggle.classList.toggle("menu-burger-is-open");
-
+    
   });
+})();
+
+(() => {
+  
+  const refs = {
+    closeMenuBrgBtn: document.querySelector("[data-menu-close-button]"),
+    menuBrg: document.querySelector("[data-menu]"),
+    bodyClassToggle: document.querySelector("body"),
+  };
+
+  refs.closeMenuBrgBtn.addEventListener("click", toggleModal);
+
+  function toggleModal() {
+    refs.menuBrg.classList.toggle("is-open"),
+    refs.bodyClassToggle.classList.toggle("menu-burger-is-open");
+    
+  }
+  
+})();
+
+(() => {
+    const openBtn = document.querySelector("[data-menu-button]");
+    const closeBtnMenuBurg = document.querySelector("[data-menu-close-button]");
+
+    closeBtnMenuBurg.addEventListener("click", () => {
+    const expanded =
+    openBtn.getAttribute("aria-expanded");
+    openBtn.setAttribute("aria-expanded", !expanded);
+});
 })();
